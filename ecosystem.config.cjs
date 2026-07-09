15cc92fc7eb602745627e5e8c4c02bd5487bbd32
mjmokhtar <mjmmiat01@gmail.com>
2026-07-09 22:54:47 +0700

    pm2 update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,16 +2,19 @@ module.exports = {
   apps: [
     {
       name: 'oldbook-chess',
-      script: 'npm',
-      args: 'run preview -- --host 0.0.0.0 --port 4173',
-      cwd: __dirname,
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '300M',
+      script: 'npx',
+      args: 'serve -s dist -l 4173 --single',
+      cwd: './',
       env: {
         NODE_ENV: 'production',
       },
+      error_file: './logs/error.log',
+      out_file: './logs/out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      merge_logs: true,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '200M',
     },
-  ],
+  ]
 }
