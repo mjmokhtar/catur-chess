2106105085b8f4eb820b0f57a136e14fe481f264
mjmokhtar <mjmmiat01@gmail.com>
2026-07-09 22:18:11 +0700

    chess.mj

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: ['chess.mjmokhtar.cloud'],
+  },
   server: {
     headers: {
-      // Tidak wajib untuk lite-single, tapi bagus disiapkan
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp',
     }
