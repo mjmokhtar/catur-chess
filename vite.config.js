9e53bf39ee00c76498e6c32d6f62bb34f27f702f
mjmokhtar <mjmmiat01@gmail.com>
2026-07-09 23:07:13 +0700

    pm2 update

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  preview: {
-    host: '0.0.0.0',
-    port: 4173,
-    allowedHosts: ['chess.mjmokhtar.cloud'],
-  },
+  base: '/',
   server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-    }
-  }
+    host: true,
+  },
+  plugins: [
+    vue(),
+  ],
 })
